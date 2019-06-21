a2d7a3474a375f71d5ffac2ad7bec055a0181f67
Gollamudi Savitha Rakendu Sri <31612459+savitha-gollamudi@users.noreply.github.com>
2019-06-21 09:45:06 +0530

    Updated with the latest code

--- Programming/Math/Examples/BinaryRepresentation.java
+++ Programming/Math/Examples/BinaryRepresentation.java
@@ -1,6 +1,6 @@
 public class Solution {
     public String findDigitsInBinary(int A) {
-        StringBuilder result = "";
+        String result = "";
         int rem = 0; 
         if(A==0) result = 0 +"";
         while(A>0){
@@ -8,10 +8,10 @@ public class Solution {
             result = result + rem;
             A = A/2;
         }
-        char[] rs = new char[result.length()];
-        for(int i = result.length()-1; i >= 0; i--){
-            rs[i]
-        }
-        return result = result.reverse();
+        
+        String reverse = "";
+        for(int i = result.length()-1; i >= 0; i-- )
+            reverse = reverse + result.charAt(i);
+        return reverse;
     }
 }
